ad0ae3cc9871d3713384a80c18634a3d34aa1ce4
Gowtham Prasad <50653813+GowthamPrasad@users.noreply.github.com>
2020-10-22 20:19:57 +0530

    Delete yield.py

--- yield.py
+++
@@ -1,8 +0,0 @@
-#yield is used inside a function like a return statement, returns a generator
-def generator():
-    for i in range(6):
-        yield i*i
-
-a = generator()
-for i in a:
-    print(i)
