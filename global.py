86c5d08f67fdfdf237f254584681d14e6b9748c2
Gowtham Prasad <50653813+GowthamPrasad@users.noreply.github.com>
2020-10-22 20:14:55 +0530

    Delete global.py

--- global.py
+++
@@ -1,9 +0,0 @@
-#global is used to declare the variable in the global scope
-def hw():
-  global a
-  a = "Hello World"
-  b = "Hello Python"
-
-hw()
-print(a)
-print(b) #it throws an exception NameError as it is defined only inside hw()
