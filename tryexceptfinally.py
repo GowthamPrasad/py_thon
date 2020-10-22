e517318157a1001bcb330403ad7d56aee4b40a96
Gowtham Prasad <50653813+GowthamPrasad@users.noreply.github.com>
2020-10-22 20:19:24 +0530

    Delete tryexceptfinally.py

--- tryexceptfinally.py
+++
@@ -1,9 +0,0 @@
-#try is used to test a block of code
-try:
-    x=open("hw.txt","r")
-#if exception occurs, except block is executed
-except:
-    print("File not found...Must've deleted or moved")
-#no matter what the outcome of try...except, finally will be executed
-finally:
-    print("Task Complete")
